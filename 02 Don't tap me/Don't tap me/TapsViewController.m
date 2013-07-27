afeff16c685bd1c4a6fbc83688cf2a1e471fef60
Sergey Surganov <surganov@me.com>
2013-07-28 00:06:55 +0400

    finished project #2

--- 02 Don't tap me/Don't tap me/TapsViewController.m
+++ 02 Don't tap me/Don't tap me/TapsViewController.m
@@ -18,14 +18,54 @@
 {
 	UITapGestureRecognizer *singleFingerTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleSingleTap:)];
 	[self.view addGestureRecognizer:singleFingerTap];
+	
+	self.view.backgroundColor = [UIColor blackColor];
+	
+	self.phrases.text = @"Don't tap me.\n\n";
 }
 
 - (void)handleSingleTap:(UITapGestureRecognizer *)recognizer
 {
 //	CGPoint location = [recognizer locationInView:[recognizer.view superview]];
-	NSLog(@"TAPPED");
+//	NSLog(@"TAPPED");
 
-	// DO SOMETHING
+//	self.phrases.text = @"You tapped me!";
+	
+	self.phrases.text = [self pickPhrase];
+}
+
+- (NSString *)pickPhrase
+{
+	NSArray *phrasesStorage = @[
+		@"I have nothing for you.\n\n",
+		@"You can go home now.\n\n",
+		@"I told you enough.\n\n",
+		@"You haven't done\nyour previous exercise.",
+		@"It was very nice to meet you.\n\n",
+		@"Leave me alone, please.\n\n",
+		@"I need some rest.\n\n",
+		@"Oh, it's you again.\n\n",
+		@"You can close the app now.\n\n",
+		@"Go and do some work.\n\n",
+		@"Stop tap like crazy!\nDo you think I'm iron?",
+		@"Come later.\n\n",
+		@"Easy there!\n\n",
+		@"I told you, I have\nnothing for you!\n\n",
+		@"Why it is do hard to understand?\n\n",
+		@"I spell it for you:\nG E T  O F F.",
+		@"You can quit now.\n\n",
+		@"You make me really angry!\n\n",
+		@"Ok, you got me. Happy?\n\n",
+		@"You won.\n\n",
+		@"This is the last time.\n\n",
+		@"Don't say anybody.\n\n",
+		@"It's over for today.\n\n",
+		@"Isn't it enough for you?\n\n"
+		];
+	
+	uint32_t randomNumber = arc4random_uniform([phrasesStorage count]);
+	
+	return [phrasesStorage objectAtIndex:randomNumber];
 }
 
 @end
