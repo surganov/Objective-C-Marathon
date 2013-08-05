3940f1c67da38ec833fbaed40503a9470ac57f4d
Sergey Surganov <surganov@me.com>
2013-08-06 00:07:17 +0400

    mickey clock, tick-tack\!

---
+++ 12 Mickey Clock/Mickey Clock/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Mickey Clock
+//
+//  Created by Sergey Surganov on 06.08.13.
+//  Copyright (c) 2013 Sergey Surganov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
